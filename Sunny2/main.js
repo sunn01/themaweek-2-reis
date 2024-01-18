17eccbe44c9c7f5242dfa33dedf0d91ecc9f4feb
sunn01 <302854486@student.rocmondriaan.nl>
2024-01-18 12:59:28 +0100

    reis themaweek 2

--- Sunny2/main.js
+++ Sunny2/main.js
@@ -3,3 +3,9 @@ const departureButton = document.querySelector('.departure-btn');
 const arrivalButton = document.querySelector('.arrival-btn');
 const searchButton = document.querySelector('.search-btn');
 
+let dropdownBtn = dosument.querySelector('.dropbtn');
+let dropdownContent = document.querySelector('.dropdown-content');
+
+dropdownBtn.addeventlistener('click', function() {
+    
+})
